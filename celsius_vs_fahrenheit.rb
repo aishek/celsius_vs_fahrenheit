feb567b276a0431b6d35d36953fd7df71f6df55a
Alexandr Borisov <ab@cifronomika.ru>
2020-02-27 22:37:51 +0300

    Add int degrees display if available

--- celsius_vs_fahrenheit.rb
+++ celsius_vs_fahrenheit.rb
@@ -2,4 +2,4 @@ puts 'Input degrees °C:'
 celsius_degrees = gets.to_i
 fahrenheit_degrees = celsius_degrees * 1.8 + 32
 
-puts "#{fahrenheit_degrees}°F"
+puts "#{fahrenheit_degrees.to_i == fahrenheit_degrees ? fahrenheit_degrees.to_i : fahrenheit_degrees}°F"
