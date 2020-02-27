3286cf70328b8689c966e6309fc5f913b4a714e4
Alexandr Borisov <ab@cifronomika.ru>
2020-02-27 22:40:51 +0300

    Add comma style for float input

--- celsius_vs_fahrenheit.rb
+++ celsius_vs_fahrenheit.rb
@@ -1,5 +1,5 @@
 puts 'Input degrees °C:'
-celsius_degrees = gets.to_i
+celsius_degrees = gets.gsub(',', '.').to_f
 fahrenheit_degrees = celsius_degrees * 1.8 + 32
 
 puts "#{fahrenheit_degrees.to_i == fahrenheit_degrees ? fahrenheit_degrees.to_i : fahrenheit_degrees}°F"
