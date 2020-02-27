0a2a2e5e7ef5287e7d32cde0c0cb7819e66c43e1
Alexandr Borisov <ab@cifronomika.ru>
2020-02-27 22:53:49 +0300

    Add F input to C conversion

--- celsius_vs_fahrenheit.rb
+++ celsius_vs_fahrenheit.rb
@@ -1,5 +1,13 @@
-puts 'Input degrees °C:'
-celsius_degrees = gets.gsub(',', '.').to_f
-fahrenheit_degrees = celsius_degrees * 1.8 + 32
+puts 'Input degrees ° (C or F at the end):'
+input = gets
+if input.end_with?("C\n")
+  celsius_degrees = input.gsub(',', '.').to_f
+  fahrenheit_degrees = celsius_degrees * 1.8 + 32
 
-puts "#{fahrenheit_degrees.to_i == fahrenheit_degrees ? fahrenheit_degrees.to_i : fahrenheit_degrees}°F"
+  puts "#{fahrenheit_degrees.to_i == fahrenheit_degrees ? fahrenheit_degrees.to_i : fahrenheit_degrees}°F"
+else
+  fahrenheit_degrees = input.gsub(',', '.').to_f
+  celsius_degrees = (fahrenheit_degrees - 32) * 5/9
+
+  puts "#{celsius_degrees.to_i == celsius_degrees ? celsius_degrees.to_i : celsius_degrees}°C"
+end
